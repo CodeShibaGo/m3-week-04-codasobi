eb0a165ec32a8a9c10a4e01218aff2673b036552
codasobi <154930553+codasobi@users.noreply.github.com>
2024-06-19 22:28:56 +0000

    feat: add line bot and open ai

--- app.py
+++ app.py
@@ -13,14 +13,21 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv('YOUR_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('YOUR_CHANNEL_SECRET'))
 
-# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 @app.route('/', methods=['GET'])
 def home():
     return 'WELCOME TO HOME PAGE'
 
 def generate_response(prompt, role="user"):
-    pass
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -38,10 +45,25 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    msg = event.message.text.lower()
+    # echo msg
+    if msg.startswith('/echo '):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=msg[6:])
+        )
+    # chatgbt
+    elif msg.startswith('/ask '):
+        completion = generate_response(text=msg[5:])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=completion)
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='This is out of my knowledge, sorry')
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
